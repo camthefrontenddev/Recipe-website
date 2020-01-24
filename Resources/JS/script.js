c26b92b2db231977fb72c2740245777008beb0b9
Cameron Beck <cambeckmarketing@gmail.com>
2020-01-23 20:34:55 -0700

    add resources

---
+++ Resources/JS/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function()  {
+    
+    /***** for the sticky nav ****/
+     $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+                $('nav').addClass('sticky');
+            } else {
+                $('nav').removeClass('sticky');
+       } 
+     }, {
+  offset: '60px'
+ });
+    
+    
+    
+ /****  *****/
+    
+    $('.js--scroll-to-plan').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    }); 
+    
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    
+    
+    /* Nav Scroll*/
+    
+    
+    $(function()  { 
+$('a[href*="#"]:not([href="#"]').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) { var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+            return false;
+        }
+       }
+    });
+  });  
+    
+    /* Animations on scroll */
+    
+    
+});
